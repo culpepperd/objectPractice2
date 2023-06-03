5d03a604cf5877f18294ebeb03dc7e345a2ec974
culpepperd <culpepperd2490@gmail.com>
2023-06-03 10:19:27 -0400

    first commit

---
+++ objectPractice2.js
@@ -0,0 +1,61 @@
+// taxi object
+var taxi = {
+	make: "Webville Motors",
+	model: "Taxi",
+	year: 1955,
+	color: "yellow",
+	passengers: 4,
+	convertible: false,
+	mileage: 281341
+};
+
+// cadillac object
+var cadi = {
+	make: "GM",
+	model: "Cadillac",
+	year: 1955,
+	color: "tan",
+	passengers: 5,
+	convertible: false,
+	mileage: 12892
+};
+
+// fiat object
+var fiat = {
+	make: "Fiat",
+	model: "500",
+	year: 1957,
+	color: "Medium Blue",
+	passengers: 2,
+	convertible: false,
+	mileage: 88000
+};
+
+// chevy object
+var chevy = {
+	make: "Chevy",
+	model: "Bel Air",
+	year: 1957,
+	color: "red",
+	passengers: 2,
+	convertible: false,
+	mileage: 1021
+};
+
+// prequalification function
+function prequal(car) {
+	if (car.mileage > 10000) {
+		return false;
+	} else if (car.year > 1960) {
+		return false;
+	}
+	return true;
+}
+
+var worthALook = prequal(chevy);
+
+if (worthALook) {
+	console.log("You gotta check out this " + chevy.make + " " + chevy.model + "!");
+} else {
+	console.log("You should really pass on the " + chevy.make + " " + chevy.model + "...");
+}
