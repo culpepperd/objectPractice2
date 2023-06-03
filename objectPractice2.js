a68b4dc6465722263acce48cdc9c562fe15f0e20
culpepperd <culpepperd2490@gmail.com>
2023-06-03 10:35:34 -0400

    added 2 functions and alert

--- objectPractice2.js
+++ objectPractice2.js
@@ -1,10 +1,22 @@
-var dog = {
+var jackShep = {
 	name: "Jack Shepard",
 	weight: 15,
 	breed: "Pom-Chi",
 	loves: "running"
 };
 
+function gainWeight(dog, amount) {
+	dog.weight = dog.weight + amount;
+}
+
+function loseWeight(dog, amount) {
+	dog.weight = dog.weight - amount;
+}
+
+gainWeight(jackShep, 3);
+loseWeight(jackShep, 4);
+
+alert(jackShep.name + " now weighs " + jackShep.weight + "lbs!");
 
 /* 
  * object reference for dog object
